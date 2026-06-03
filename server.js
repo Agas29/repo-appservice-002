02bf7e92dd717e8909276ddbc37b0b16714bb342
Agas17 <anggorogigis9191@gmail.com>
2026-06-03 21:43:18 +0700

    Update server message and endpoint description

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Endpoint bawaan modul
 app.get('/', (req, res) => {
     res.send('<h1>Praktikum Azure App Service</h1>');
 });
 
-// EXERCISE: Endpoint baru untuk mencetak waktu server backend
+// EXERCISE: Endpoint baru untuk mengambil waktu server
 app.get('/waktu', (req, res) => {
     res.json({
         waktuServer: new Date().toISOString()
     });
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server berjalan pada port ${port}`));
